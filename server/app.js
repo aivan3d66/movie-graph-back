96d9df83e219a76db72e17612f5fc5a95363f4ce
Ivan <adamovskij00@mail.ru>
2022-05-03 23:09:26 +0300

    add rollback the port

--- server/app.js
+++ server/app.js
@@ -27,6 +27,6 @@ const dbConnection = mongoose.connection
 dbConnection.on('error', err => console.log(`Connection error: ${err}`))
 dbConnection.once('open', () => console.log('Connected to DB!'))
 
-app.listen('https://movie-graph-server.herokuapp.com', err => {
+app.listen(port, err => {
     err ? console.log(err) : console.log('Server started!')
 })
